8df439e77a70f060784f0293de32e7292c117116
Mohsine Bakhcha <mohsine.52.work@gmail.com>
2023-03-05 16:34:49 +0100

    Re-organized files and added footer and navbar

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,11 @@
+//Website introduction Card
 const popup = document.getElementById("popup");
-        const bod = document.getElementById("body");
-        const closeBtn = document.getElementById("cls-popup");
-        document.addEventListener('DOMContentLoaded', () => {
-          popup.style.display = 'flex';
-        });
-        closeBtn.addEventListener('click', () => {
-          popup.style.display = 'none';
-          bod.style.overflowY = 'scroll';
-        });
-
-const form = document.querySelector('.login-form');
-
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    if (email === "mohsine@gmail.com" && password === "2101") {
-      window.location.href = "stg-profile.html";
-    } else {
-      alert("Email or password is incorrect. Please try again.");
-    }
-  });
+const bod = document.getElementById("body");
+const closeBtn = document.getElementById("cls-popup");
+document.addEventListener('DOMContentLoaded', () => {
+  popup.style.display = 'flex';
+});
+closeBtn.addEventListener('click', () => {
+  popup.style.display = 'none';
+  bod.style.overflowY = 'scroll';
+});
---
+++ assets/template.js
@@ -0,0 +1,19 @@
+//Link Template to website pages
+// Load navbar
+var xhttp1 = new XMLHttpRequest();
+xhttp1.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("navbar-container").innerHTML = this.responseText;
+}
+};
+xhttp1.open("GET", "/templates/navbar.html", true);
+xhttp1.send();
+// Load footer
+var xhttp2 = new XMLHttpRequest();
+xhttp2.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("footer-container").innerHTML = this.responseText;
+}
+};
+xhttp2.open("GET", "/templates/footer.html", true);
+xhttp2.send();
