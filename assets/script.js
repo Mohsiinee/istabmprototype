0e7e64c84a0d8964403825fbcc48448bcd51f208
Mohsine Bakhcha <mohsine.52.work@gmail.com>
2023-03-05 20:00:10 +0100

    Re-organized files and added footer and navbar

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,3 @@
-const links = document.querySelectorAll('a[href], link[rel="stylesheet"][href], script[src]');
-for (let i = 0; i < links.length; i++) {
-  const href = links[i].getAttribute('href') || links[i].getAttribute('src');
-  if (href && href[0] === '/') {
-    links[i].setAttribute(links[i].nodeName === 'SCRIPT' ? 'src' : 'href', '/istabmprototype' + href);
-  }
-}
 //Website introduction Card
 const popup = document.getElementById("popup");
 const bod = document.getElementById("body");
